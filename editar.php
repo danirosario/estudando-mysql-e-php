bf5dab43e5e13461c323b1ff158c8c7591ec855d
danirosario <rosario.dani01@gmail.com>
2026-06-25 22:49:55 -0300

    add repositorio local ao github

--- editar.php
+++ editar.php
@@ -9,6 +9,17 @@ if ($id <= 0) {
     exit;
 }
 
+//BUSCAR OS DADOS VIA ID
+$stmt = $conn->prepare("SELECT nome, idade, email FROM cliente WHERE id = ?");
+$stmt->bind_param("i", $id);
+$stmt->execute();
+$result = $stmt->get_result(); 
+$cliente = $result->fetch_assoc();
+if(!$cliente) {
+    echo "not found";
+    exit;
+}
+
 
 //VERIFICAR SE RECEBEMOS OS DADOS DO FORMULARIO PELO METODO POST
 
@@ -17,6 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = trim($_POST["nome"]); //trim remove espaços em branco 
     $idade = $_POST["idade"];
     $email = $_POST["email"];
+    $id = $_POST["id"];
 
     //FAZER UMA VALIDAÇÃO SIMPLES DOS DADOS RECEBIDOS
     if (empty($nome) || empty($idade) || empty($email) || !is_numeric($idade)) {
@@ -25,14 +37,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $idade = (int) $idade; //converter idade para inteiro
 
         //PREPARAR O COMANDO SQL PARA INSERÇÃO DOS DADOS
-        $stmt = $conn->prepare("INSERT INTO cliente (nome, idade, email) VALUES (?,?,?)");
-        $stmt->bind_param("sis", $nome, $idade, $email); // VINCULANDO OS PARAMETROS
+        $stmt = $conn->prepare("UPDATE cliente SET nome = ?, idade = ?, email = ? WHERE id = ?");
+        $stmt->bind_param("sisi", $nome, $idade, $email, $id); // VINCULANDO OS PARAMETROS
 
         //EXECUTAR O SQL
         if($stmt->execute()) {
-            echo "Cadastro realizado com sucesso!";
+            echo "Edição realizado com sucesso!";
         } else {
-            echo "Erro ao cadastrar: " . $stmt->error;
+            echo "Erro ao editar: " . $stmt->error;
         }
 
         //FECHAR A CONEXÃO
@@ -52,17 +64,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <body>
     <form action="" method="POST">
+        <input type="hidden" name="id" value="<?php echo $id; ?>" required>
         <label>Nome:</label>
-        <input type="text" name="nome" required>
+        <input type="text" name="nome" value="<?php echo $cliente['nome']; ?>" required>
         <br><br>
         <label>Idade:</label>
-        <input type="number" name="idade" required>
+        <input type="number" name="idade" value="<?php echo $cliente['idade']; ?>" required>
         <br><br>
         <label>Email:</label>
-        <input type="email" name="email" required>
+        <input type="email" name="email" value="<?php echo $cliente['email']; ?>" required>
         <br><br>
         <button type="submit">Salvar</button>
     </form>
 </body>
 
-</html>
+</html>  
